45e162b44ba7befcef2736ec27ab8a832efd4aac
unknown <drgs_lx@yahoo.com>
2024-02-16 14:23:17 +0200

    added not-found component and functionality to cart-page, food-page and home-page

--- src/app/not-found/not-found.component.ts
+++ src/app/not-found/not-found.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.css'
 })
-export class NotFoundComponent {
+export class NotFoundComponent implements OnInit {
+ @Input() visible:Boolean = false;
+ @Input() notFoundMessage:String = "Nothing found!";
+ @Input() resetLinkText:String = "Go to home page";
+ @Input() resetLinkRoute:String = "/";
 
+  constructor() { }
+  ngOnInit(): void {
+    
+  }
 }
